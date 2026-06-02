845d9958c06bf6752f18e1a9336aa48c786b73bc
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2026-06-02 16:24:24 -0300

    Version 0.0.3

--- src/main.py
+++ src/main.py
@@ -1,1 +1,3 @@
+from textnode import TextNode
 print("Hello World")
+TextNode("This is some anchor text", "link", "https://www.boot.dev")
--- src/textnode.py
+++ src/textnode.py
@@ -7,3 +7,17 @@ class TextTypes(Enum):
     CODE = "`Code text`"
     Links = "[anchor text](url)"
     Images = "![alt text](url)"
+
+class TextNode():
+                #textype is a member of the Enum
+    def __init__(self, text, textType, url):
+        self.text = text
+        self.textType = textType
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text:
+            return True
+        
+    def __repr__(text, textType, url):
+        return text.value, textType.value, url.value
