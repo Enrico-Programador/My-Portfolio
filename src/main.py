ee8f68e06106a08817d5cbe9c89da301e55fb9bb
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2026-06-10 17:25:51 -0300

    0.1.2
    Just minor changes to syntax but same version.

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from splitnode import split_nodes_delimiter
 from textnode import TextNode, TextTypes, text_node_to_html_node
 
 def main():
-    print(TextTypes.BOLD)
+    print("running main...")
     node = TextNode("This is text with a `code block` word", TextTypes.TEXT)
     new_nodes = split_nodes_delimiter([node], "`", TextTypes.CODE)
     
--- src/splitnode.py
+++ src/splitnode.py
@@ -3,13 +3,14 @@ from textnode import TextNode, TextTypes
 
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextTypes) -> list[TextNode]:
+
     new_nodes = []
     new_nodes_list = []
     
     for nodes in old_nodes:
         i = 0
         new_nodes.extend(nodes.text.split(delimiter))
-        print(i%2)
+        
         for new in new_nodes:
             if i%2 == 1:
                 next_node = TextNode(f"{new_nodes[i]}", text_type)
@@ -17,6 +18,6 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
                 next_node = TextNode(f"{new_nodes[i]}", TextTypes.TEXT)
             new_nodes_list.append(next_node)
             i+=1
-    print(new_nodes_list)
+    
     return new_nodes_list
     
