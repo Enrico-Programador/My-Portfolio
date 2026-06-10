63503b81d68e6c706f6fa38de388fed16ed0cd9a
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2026-06-10 17:19:56 -0300

    0.1.2
    Delimiter logic ready for new tests tomorrow.

--- src/splitnode.py
+++ src/splitnode.py
@@ -4,7 +4,19 @@ from textnode import TextNode, TextTypes
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextTypes) -> list[TextNode]:
     new_nodes = []
-    new_nodes.extend(old_nodes[0].text.split(delimiter))
-
-    print(new_nodes)
+    new_nodes_list = []
+    
+    for nodes in old_nodes:
+        i = 0
+        new_nodes.extend(nodes.text.split(delimiter))
+        print(i%2)
+        for new in new_nodes:
+            if i%2 == 1:
+                next_node = TextNode(f"{new_nodes[i]}", text_type)
+            else:
+                next_node = TextNode(f"{new_nodes[i]}", TextTypes.TEXT)
+            new_nodes_list.append(next_node)
+            i+=1
+    print(new_nodes_list)
+    return new_nodes_list
     
