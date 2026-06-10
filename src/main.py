2ba55e9f0aedfb30e60ec7b0fa733646c554b5c0
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2026-06-10 16:33:42 -0300

    Version 0.1.2

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from htmlnode import LeafNode, ParentNode
+from splitnode import split_nodes_delimiter
 from textnode import TextNode, TextTypes, text_node_to_html_node
 
 def main():
-    node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
     print(TextTypes.BOLD)
+    node = TextNode("This is text with a `code block` word", TextTypes.TEXT)
+    new_nodes = split_nodes_delimiter([node], "`", TextTypes.CODE)
     
 
 main()
--- src/splitnode.py
+++ src/splitnode.py
@@ -3,4 +3,8 @@ from textnode import TextNode, TextTypes
 
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextTypes) -> list[TextNode]:
-    pass
+    new_nodes = []
+    new_nodes.extend(old_nodes[0].text.split(delimiter))
+
+    print(new_nodes)
+    
