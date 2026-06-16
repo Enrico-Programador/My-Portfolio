352c9e8aff3510240c8774bb3eb0a8e6613168f8
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2026-06-16 16:19:23 -0300

    Version 0.1.7
    Trying a new function to split images.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from extractmarkdown import extract_markdown_images
 from htmlnode import LeafNode, ParentNode
-from splitnode import split_nodes_delimiter
+from splitnode import split_nodes_delimiter, split_nodes_image
 from textnode import TextNode, TextTypes, text_node_to_html_node
 
 def main():
     print("running main...")
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextTypes.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
+    
 main()
--- src/splitnode.py
+++ src/splitnode.py
@@ -1,4 +1,7 @@
 
+import re
+
+from extractmarkdown import extract_markdown_images
 from textnode import TextNode, TextTypes
 
 
@@ -10,7 +13,6 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
 
     for nodes in old_nodes:
         j = 0
-        
         new_nodes.extend(nodes.text.split(delimiter))
         
         for i in range(starting_index, len(new_nodes)):
@@ -28,7 +30,55 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
             raise Exception("Markdown was not closed")
                 
         starting_index = starting_index + len(new_nodes)
-    
-        
     return new_nodes_list
-    
+
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    new_nodes_list = []
+    for node in old_nodes:
+        if "image" not in node.text:
+            return [node]
+        extracted_images = extract_markdown_images(node.text)
+        i = 0
+        j = 0
+        
+        new_nodes.extend(node.text.split(f"![{extracted_images[0][0]}]({extracted_images[0][1]})", 1))
+        print(new_nodes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
+        for images in extracted_images:
+            
+            next_node = TextNode(f"{new_nodes[i]}", node.textType)
+            if next_node.text.strip() != "":
+                new_nodes_list.append(next_node)
+
+            next_node = TextNode(f"{images[1]}", TextTypes.IMAGE, f"{images[0]}")
+            if next_node.text.strip() != "":
+                new_nodes_list.append(next_node)
+            
+            j += 1
+            i += 1
+            '''
+            
