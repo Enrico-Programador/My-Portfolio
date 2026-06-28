9f883d6d124c43ab5c205b9e121ad59916b2e698
Enrico-Programador <enrico2015ap@gmail.com>
2026-06-28 13:13:27 -0300

    Version 0.2.4

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,17 +1,18 @@
 from ctypes.wintypes import POINT
 
 from block_markdown import BlockTypes, block_to_block_type, markdown_to_blocks
-from splitnode import split_nodes_delimiter
-from textnode import TextTypes
+from splitnode import split_nodes_delimiter, text_to_textnodes
+from textnode import TextTypes, text_node_to_html_node
 
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
+    final = ""
     for block in blocks:
         block_type = block_to_block_type(block)
-        print(block)
-        chama = split_nodes_delimiter([block], "**", TextTypes.TEXT)
-        print(chama)
-        if block_type is BlockTypes.PARAGRAPH:
-            tag = "<p>"
-    return blocks
+        split = text_to_textnodes(block)
+        print(split)
+        for nodes in split:
+            leaf = text_node_to_html_node(nodes)
+            final = final + leaf.to_html()
+    return final
