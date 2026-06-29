030735baf7b542b4f09e819ea822ee7bf98f393b
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2026-06-29 10:01:43 -0300

    Version 0.2.4 - Still working on markdown to HTML

--- src/main.py
+++ src/main.py
@@ -9,13 +9,15 @@ This is **bolded** paragraph
 text in a p
 tag here
 
+
+
 This is another paragraph with _italic_ text and `code` here
 
 """
 
     node = markdown_to_html_node(md)
-    
-    print(f"Return value: {node}")
+    html = node.to_html()
+    print(f"Return value: {html}")
     print("Expected return value: <div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>")
 
 main()
--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,5 +1,3 @@
-from ctypes.wintypes import POINT
-
 from block_markdown import BlockTypes, block_to_block_type, markdown_to_blocks
 from htmlnode import ParentNode
 from splitnode import text_to_textnodes
@@ -8,19 +6,23 @@ from textnode import TextTypes, text_node_to_html_node
 
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
-    final = "<div>"
-    
+    return_list = []
     print(f"Blocks we are working with: {blocks}")
+
     for block in blocks:
         block_type = block_to_block_type(block)
-        split = text_to_textnodes(block)
-        nodes_list = []
-        for nodes in split:
-            leaf = text_node_to_html_node(nodes)
-            
-            nodes_list.append(leaf)
 
         if block_type == BlockTypes.PARAGRAPH:
-            final = final + ParentNode("p", nodes_list).to_html().replace('\n', ' ') 
+            return_list.append(block_type_paragraph(block))
             
-    return final+"</div>"
+    return ParentNode("div", return_list)
+
+
+def block_type_paragraph(block):
+    split = text_to_textnodes(block.replace('\n', ' '))
+    nodes_list = []
+    for nodes in split:
+        leaf = text_node_to_html_node(nodes)
+        nodes_list.append(leaf)
+
+    return ParentNode("p", nodes_list)
